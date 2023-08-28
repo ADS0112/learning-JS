60b7213aadcbc24176744a72fe499113bab66524
ADS0112 <136665263+ADS0112@users.noreply.github.com>
2023-08-28 19:58:20 -0400

    Moving on to fundamentals 2
    Knew most of fundamentals 1, didn't do much

---
+++ Fundamentals 1/script.js
@@ -0,0 +1,44 @@
+// Logical operators
+
+// const hasDL = true;
+// const hasGV = true;
+
+// console.log(hasDL && hasGV);
+
+//Switch statments
+
+// const day = "tuesday";
+
+// switch (day) {
+//   case "monday":
+//     console.log("hey");
+//     break;
+//   case "tuesday":
+//     console.log("hey2");
+//     break;
+//   case "wednesday":
+//     console.log("hey23");
+//     break;
+//   case "monday":
+//     console.log("hey");
+//     break;
+// }
+
+////////////////////////////////////
+// The Conditional (Ternary) Operator
+// const age = 23;
+// // age >= 18 ? console.log('I like to drink wine 🍷') : console.log('I like to drink water 💧');
+
+// const drink = age >= 18 ? 'wine 🍷' : 'water 💧';
+// console.log(drink);
+
+// let drink2;
+// if (age >= 18) {
+//   drink2 = 'wine 🍷';
+// } else {
+//   drink2 = 'water 💧';
+// }
+// console.log(drink2);
+
+// console.log(`I like to drink ${age >= 18 ? 'wine 🍷' : 'water 💧'}`);
+//
