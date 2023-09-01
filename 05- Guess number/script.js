06f30da7f5b986acf840e5c562b37fb4c5358d29
ADS0112 <136665263+ADS0112@users.noreply.github.com>
2023-09-01 19:49:16 -0400

    adding click events to check for scores and updating scores

--- 05- Guess number/script.js
+++ 05- Guess number/script.js
@@ -3,3 +3,36 @@
 // document.querySelector('.message').textContent = 'Correct Number';
 
 // document.querySelector('.number').textContent = 13;
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+document.querySelector('.number').textContent = secretNumber;
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'Please Pick a Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '✔️ Correct Number! ';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '❌ Too High!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '👎 You Lose The Game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '❌ Too Low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '👎 You Lose The Game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
