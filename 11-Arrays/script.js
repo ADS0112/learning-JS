0884b18cef54b9af972366862226207871a4c588
ADS0112 <136665263+ADS0112@users.noreply.github.com>
2023-09-17 17:40:25 -0400

    Update script.js

--- 11-Arrays/script.js
+++ 11-Arrays/script.js
@@ -522,24 +522,24 @@ btnSort.addEventListener('click', function (e) {
 
 //Challenge 4
 
-const dogs = [
-  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
-  { weight: 8, curFood: 200, owners: ['Matilda'] },
-  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
-  { weight: 32, curFood: 340, owners: ['Michael'] },
-];
+// const dogs = [
+//   { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+//   { weight: 8, curFood: 200, owners: ['Matilda'] },
+//   { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+//   { weight: 32, curFood: 340, owners: ['Michael'] },
+// ];
 
-dogs.forEach(dog => (dog.recFood = Math.trunc(dog.weight ** 0.75 * 28)));
+// dogs.forEach(dog => (dog.recFood = Math.trunc(dog.weight ** 0.75 * 28)));
 
-console.log(dogs);
-// console.log(dogs.map(dog => dog.weight));
+// console.log(dogs);
+// // console.log(dogs.map(dog => dog.weight));
 
-//2
+// //2
 
-const dogSarah = dogs.find(dog => dog.owners.includes('Sarah'));
+// const dogSarah = dogs.find(dog => dog.owners.includes('Sarah'));
 
-console.log(
-  `Sarah's dog is eating too ${
-    dogSarah.curFood > dogSarah.recFood ? 'much' : 'little'
-  } `
-);
+// console.log(
+//   `Sarah's dog is eating too ${
+//     dogSarah.curFood > dogSarah.recFood ? 'much' : 'little'
+//   } `
+// );
