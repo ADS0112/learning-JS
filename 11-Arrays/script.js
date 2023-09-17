7c66bbfac4bdf6bd427c08397f0b075f1e4d424f
ADS0112 <136665263+ADS0112@users.noreply.github.com>
2023-09-17 17:32:20 -0400

    challenge was hard
    blanked on everything

--- 11-Arrays/script.js
+++ 11-Arrays/script.js
@@ -529,4 +529,17 @@ const dogs = [
   { weight: 32, curFood: 340, owners: ['Michael'] },
 ];
 
-console.log(dogs.map(dog => dog.weight));
+dogs.forEach(dog => (dog.recFood = Math.trunc(dog.weight ** 0.75 * 28)));
+
+console.log(dogs);
+// console.log(dogs.map(dog => dog.weight));
+
+//2
+
+const dogSarah = dogs.find(dog => dog.owners.includes('Sarah'));
+
+console.log(
+  `Sarah's dog is eating too ${
+    dogSarah.curFood > dogSarah.recFood ? 'much' : 'little'
+  } `
+);
