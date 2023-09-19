54f0ea0a99d487fbfadb2f87e230d70f3ca26d3a
ADS0112 <136665263+ADS0112@users.noreply.github.com>
2023-09-19 15:21:43 -0400

    Adding smooth scrolling
    learning about scrolling while taking into current and relavtive posioton

--- 13-Advanced Dom/script.js
+++ 13-Advanced Dom/script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +19,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +29,49 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+
+///////LECTURE
+
+//Creating and inserting elements
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.textContent = 'We use cookies'; 
+
+// .prepend adds the elemnt as the first child of the element and .append adds it as the last child
+//Insert multiple copes of the same element
+//header.append(message.cloneNode(true));
+
+//Before and after
+//header.before(message) - before the header
+//header.after(message)
+
+///removing elements
+//.remove
+
+//Styles
+//getComputedStyle((message).color) - contains all the properties unless we specify like .color
+
+// document.documentElement.style.setProperty('--color-primary', 'orangered')
+
+//Attributes 
+// const logo = document.querySelector('nav__logo');
+// //Can get default properties 
+// console.log(logo.alt);
+
+// //Can get custom properties
+// console.log(logo.getAttribute('custon-name'));
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click',function (e){
+  const s1Coords = section1.getBoundingClientRect();
+  console.log(s1Coords);
+
+  //Scrolling
+  window.scrollTo({
+    left: s1Coords.left  + window.scrollX, top: s1Coords.top + window.scrollY,
+    behaviour: 'smooth',
+  });
