d098d952e3045441407c16833d22ae1466927cfb
ADS0112 <136665263+ADS0112@users.noreply.github.com>
2023-09-21 12:41:36 -0400

    implemented lazy loading

--- 13-Advanced Dom/script.js
+++ 13-Advanced Dom/script.js
@@ -159,7 +159,6 @@ const allSections = document.querySelectorAll('.section');
 
 const revaelSection = function(entires, observer) {
   const [entry] = entires;
-  console.log(entry)
   if(!entry.isIntersecting) return;
   entry.target.classList.remove('section--hidden');
   observer.unobserve(entry.target);
@@ -178,8 +177,29 @@ allSections.forEach(section => {
 
 //Lazy Loading 
 const imgTargets = document.querySelectorAll('img[data-src]');
-console.log(imgTargets)
 
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  // Replace src with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
 ///////LECTURE////////////////////////////////
 
 //Creating and inserting elements
