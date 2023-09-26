8527f73f6a74fa7e0aafd1df387c7f36d6e51834
ADS0112 <136665263+ADS0112@users.noreply.github.com>
2023-09-26 14:58:08 -0400

    Update script.js
    setting child classes to prototypeof parent calss

--- 14 - OOP/script.js
+++ 14 - OOP/script.js
@@ -130,3 +130,61 @@
 // console.log(ford.speedUS);
 // ford.speedUS = 70;
 // console.log(ford)
+
+// const Person = function (firstName, birthYear) {
+//     //Instnace properties, will be available on the instances that are created, such as me
+//     this.firstName = firstName;
+//     this.birthYear = birthYear;
+//   };
+
+//   Person.prototype.calcAge = function () {
+//   console.log(2037 - this.birthYear);
+// };
+
+// const Student = function(firstName, birthYear, course) {
+//   Person.call(this, firstName, birthYear);
+//   this.course = course;
+// }
+
+// //Linking prootostypes allows for instances of student to access functions of parent person class 
+// Student.prototype = Object.create(Person.prototype);
+
+
+// Student.prototype.constructor = Student;
+
+// const mike = new Student('Mike', 2020, 'CS');
+// mike.calcAge();
+
+
+//Challenege 3
+const Car = function(make, speed) {
+  this.make = make;
+  this.speed = speed;
+}
+
+
+Car.prototype.brake = function(speed) {
+  this.speed -= 10;
+}
+
+
+const EV = function(make, speed, charge) {
+  Car.call(this, make, speed);
+  this.charge = charge;
+}
+
+EV.prototype = Object.create(Car.prototype);
+
+EV.prototype.chargeBattery = function(chargeTo) {
+  this.charge = chargeTo;
+}
+EV.prototype.accelerate = function() {
+  this.speed += 20;
+  this.charge--;
+  console.log(`${this.make}, is going ${this.speed}. with ${this.charge}% Battery`);
+}
+
+const tesla = new EV('Tesla', 120, 23);
+tesla.chargeBattery(90);
+console.log(tesla)
+tesla.accelerate();
