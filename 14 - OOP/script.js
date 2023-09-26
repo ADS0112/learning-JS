86f1da32b32f65a399514cc7b9f71593e7894f59
ADS0112 <136665263+ADS0112@users.noreply.github.com>
2023-09-26 12:45:40 -0400

    Update script.js
    setters and getters, learned about es6 classes

--- 14 - OOP/script.js
+++ 14 - OOP/script.js
@@ -1,3 +1,4 @@
+
 'use strict';
 //Construcor functions
 
@@ -20,3 +21,65 @@ Person.prototype.calcAge = function () {
   console.log(2037 - this.birthYear);
 };
 me.calcAge();
+
+
+//Challeneg 1
+
+// const Car = function(make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// }
+
+// Car.prototype.accelerate = function(speed) {
+//   this.speed += 10;
+// }
+// Car.prototype.brake = function(speed) {
+//   this.speed -= 10;
+// }
+// const car1 = new Car('BMW', 120);
+// console.log(car1);
+
+// car1.brake();
+// console.log(car1)
+
+//Es6 Classes
+
+
+//Class expression
+// class PersonCl {
+// constructor(firstName, birthYear) {
+//   this.firstName = firstName;
+//   this.birthYear = birthYear;
+// } 
+
+// calcAge() {
+//   console.log(2037 - this.birthYear);
+// }
+// }
+
+// const jessica = new PersonCl('Jessica', 1990);
+
+//Class declaration
+// const PersonCl = class {};
+
+//Setters and Getters
+
+const account = {
+  owner: 'joe',
+  movements: [200, 300, 400, 200],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+
+  },
+  set latest(mov) {
+    this.movements.push(mov);
+  }
+}
+
+
+//Get and Set as now read as properites instead of calling the method
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements)
